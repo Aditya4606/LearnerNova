1a5ce2d3f3addbdd50a04b3909912566c86f9b9d
jaimin <jaiminparmar200606@gmail.com>
2026-03-21 13:10:30 +0530

    backend setup done

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+import authRoutes from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+// Middleware
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running 🚀")
+})
+
+
+
+app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
